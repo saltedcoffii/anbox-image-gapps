652a7d0c8d49f435ac795437b0e0a47b2f041eab
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-15 08:35:53 -0400

    Update URLs

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ export _gapps_rel="$(curl -sL https://api.opengapps.org/list | sed -r 's/.*-x86_
 export _gapps_src="https://downloads.sourceforge.net/project/opengapps/x86_64/$_gapps_rel/open_gapps-x86_64-7.1-pico-$_gapps_rel.zip"
 
 busybox wget "https://build.anbox.io/android-images/$imgver2/android_amd64.img"
-busybox wget "https://github.com/redchenjs/aur-packages/raw/master/anbox-image/houdini_y.sfs"
-busybox wget "https://github.com/redchenjs/aur-packages/raw/master/anbox-image/houdini_z.sfs"
+busybox wget "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_y.sfs"
+busybox wget "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_z.sfs"
 busybox wget "$_gapps_src"
 busybox wget "$_gapps_src.md5"
