c306d24bd96f9097ebfed3ab2d397df10c6f18c0
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-15 22:42:08 +0000

    remove busybox dependency

--- download.sh
+++ download.sh
@@ -6,8 +6,8 @@ export imgver2=${imgver//./\/}
 export _gapps_rel="$(curl -sL https://api.opengapps.org/list | sed -r 's/.*-x86_64-7.1-pico-([0-9]+).zip".*/\1/')"
 export _gapps_src="https://downloads.sourceforge.net/project/opengapps/x86_64/$_gapps_rel/open_gapps-x86_64-7.1-pico-$_gapps_rel.zip"
 
-busybox wget "https://build.anbox.io/android-images/$imgver2/android_amd64.img"
-busybox wget "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_y.sfs"
-busybox wget "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_z.sfs"
-busybox wget "$_gapps_src"
-busybox wget "$_gapps_src.md5"
+curl -#LO "https://build.anbox.io/android-images/$imgver2/android_amd64.img"
+curl -#LO "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_y.sfs"
+curl -#LO "https://github.com/redchenjs/aur-packages/releases/download/anbox-image/houdini_z.sfs"
+curl -#LO "$_gapps_src"
+curl -#LO "$_gapps_src.md5"
